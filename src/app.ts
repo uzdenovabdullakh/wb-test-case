c8446ed184ac35c21fd208b89a8ec887d7549e43
uzdenovabdullakh <uzdenovabdullah@yandex.ru>
2025-06-29 12:42:01 +0300

    feat: add scheduler

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,18 @@
 import knex, { migrate, seed } from "#postgres/knex.js";
+import { startSchedulers } from "#scheduler.js";
 
-await migrate.latest();
-await seed.run();
+async function main(): Promise<void> {
+    try {
+        await migrate.latest();
+        await seed.run();
+        console.log("Database initialized");
 
-console.log("All migrations and seeds have been run");
+        startSchedulers();
+        console.log("Schedulers started");
+    } catch (error) {
+        console.error("Application failed to start", error);
+        process.exit(1);
+    }
+}
+
+main();
